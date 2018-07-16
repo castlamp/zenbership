b4f4d3a262fd59e0714ed792ad937ae1942ec6e2
jbelelieu <jonnyobell@gmail.com>
2018-07-16 12:46:09 -0400

    Updated state list with new countries added

---
+++ admin/sd-system/state_list.php
@@ -0,0 +1,280 @@
+<?php
+
+$states           = array();
+
+// ==================================
+//   Start Editing here!
+
+// ----------------------------------
+//   United States
+
+$states["GROUP1"] = "American States";
+$states["AL"]     = "Alabama";
+$states["AK"]     = "Alaska";
+$states["AS"]     = "American Samoa";
+$states["AZ"]     = "Arizona";
+$states["AR"]     = "Arkansas";
+$states["CA"]     = "California";
+$states["CO"]     = "Colorado";
+$states["CT"]     = "Connecticut";
+$states["DE"]     = "Delaware";
+$states["DC"]     = "District of Columbia";
+$states["FL"]     = "Florida";
+$states["GA"]     = "Georgia";
+$states["GU"]     = "Guam";
+$states["HI"]     = "Hawaii";
+$states["ID"]     = "Idaho";
+$states["IL"]     = "Illinois";
+$states["IN"]     = "Indiana";
+$states["IA"]     = "Iowa";
+$states["KS"]     = "Kansas";
+$states["KY"]     = "Kentucky";
+$states["LA"]     = "Louisiana";
+$states["ME"]     = "Maine";
+$states["MH"]     = "Marshall Islands";
+$states["MD"]     = "Maryland";
+$states["MA"]     = "Massachusetts";
+$states["MI"]     = "Michigan";
+$states["MN"]     = "Minnesota";
+$states["MS"]     = "Mississippi";
+$states["MO"]     = "Missouri";
+$states["MT"]     = "Montana";
+$states["NE"]     = "Nebraska";
+$states["NV"]     = "Nevada";
+$states["NH"]     = "New Hampshire";
+$states["NJ"]     = "New Jersey";
+$states["NM"]     = "New Mexico";
+$states["NY"]     = "New York";
+$states["NC"]     = "North Carolina";
+$states["ND"]     = "North Dakota";
+$states["MP"]     = "Northern Mariana Islands";
+$states["OH"]     = "Ohio";
+$states["OK"]     = "Oklahoma";
+$states["OR"]     = "Oregon";
+$states["PW"]     = "Palau";
+$states["PA"]     = "Pennsylvania";
+$states["PR"]     = "Puerto Rico";
+$states["RI"]     = "Rhode Island";
+$states["SC"]     = "South Carolina";
+$states["SD"]     = "South Dakota";
+$states["TN"]     = "Tennessee";
+$states["TX"]     = "Texas";
+$states["UT"]     = "Utah";
+$states["VT"]     = "Vermont";
+$states["VI"]     = "Virgin Islands";
+$states["VA"]     = "Virginia";
+$states["WA"]     = "Washington";
+$states["WV"]     = "West Virginia";
+$states["WI"]     = "Wisconsin";
+$states["WY"]     = "Wyoming";
+$states["AE"]     = "Armed Forces (Africa, Canada, Europe, Middle East)";
+$states["AA"]     = "Armed Forces Americas";
+$states["AP"]     = "Armed Forces Pacific";
+
+
+// ----------------------------------
+//   Canada
+$states["xx1"]    = "";
+$states["GROUP2"] = "Canadian Provinces";
+$states["AB"]     = "Alberta";
+$states["BC"]     = "British Columbia";
+$states["MB"]     = "Manitoba";
+$states["NB"]     = "New Brunswick";
+$states["NL"]     = "Newfoundland and Labrador";
+$states["NT"]     = "Northwest Territories";
+$states["NS"]     = "Nova Scotia";
+$states["NU"]     = "Nunavut";
+$states["ON"]     = "Ontario";
+$states["PE"]     = "Prince Edward Island";
+$states["QC"]     = "Quebec";
+$states["SK"]     = "Saskatchewan";
+$states["YT"]     = "Yukon";
+
+
+// ----------------------------------
+//   Australia
+
+$states["xx2"]    = "";
+$states["GROUP3"] = "Australian States";
+$states["ACT"]    = "Australian Capital Territory";
+$states["NTA"]    = "Northern Territory"; // NT
+$states["NSW"]    = "New South Wales";
+$states["QLD"]    = "Queensland";
+$states["SA"]     = "South Australia";
+$states["TAS"]    = "Tasmania";
+$states["VIC"]    = "Victoria";
+$states["AWA"]    =  "Western Australia";
+
+
+// ----------------------------------
+//   South Africa
+
+$states["xx5"]    = "";
+$states["GROUP3"] = "South African States";
+$states["Eastern Cape"]    = "Eastern Cape";
+$states["Free State"]    = "Free State";
+$states["Gauteng"]    = "Gauteng";
+$states["KwaZulu-Natal"]    = "KwaZulu-Natal";
+$states["Limpopo"]    = "Limpopo";
+$states["Mpumalanga"]    = "Mpumalanga";
+$states["Northern Cape"]    = "Northern Cape";
+$states["North West"]    = "North West";
+$states["Western Cape"]    = "Western Cape";
+
+
+// ----------------------------------
+//   New Zealand
+
+$states["xx77"]    = "";
+$states["GROUP3"] = "New Zealand States";
+$states["Auckland"]    = "Auckland";
+$states["Bay of Plenty"]    = "Bay of Plenty";
+$states["Canterbury"]    = "Canterbury";
+$states["Gisborne"]    = "Gisborne";
+$states["Hawke's Bay"]    = "Hawke's Bay";
+$states["Marlborough"]    = "Marlborough";
+$states["Manawatu-Wanganui"]    = "Manawatu-Wanganui";
+$states["Nelson"]    = "Nelson";
+$states["Northland"]    = "Northland";
+$states["Otago"]    = "Otago";
+$states["Southland"]    = "Southland";
+$states["Tasman"]    = "Tasman";
+$states["Taranaki"]    = "Taranaki";
+$states["Waikato"]    = "Waikato";
+$states["Wellington"]    = "Wellington";
+$states["West Coast"]    = "West Coast";
+$states["Chatham Islands"]    = "Chatham Islands";
+
+
+// ----------------------------------
+//   England
+
+$states["xx78"]    = "";
+$states["GROUP4"] = "England";
+$states["Avon"] = 'Avon';
+$states["Bedfordshire"] = 'Bedfordshire';
+$states["Berkshire"] = 'Berkshire';
+$states["Buckinghamshire"] = 'Buckinghamshire';
+$states["Cambridgeshire"] = 'Cambridgeshire';
+$states["Cheshire"] = 'Cheshire';
+$states["Cleveland"] = 'Cleveland';
+$states["Cornwall"] = 'Cornwall';
+$states["Cumbria"] = 'Cumbria';
+$states["Derbyshire"] = 'Derbyshire';
+$states["Devon"] = 'Devon';
+$states["Dorset"] = 'Dorset';
+$states["Durham"] = 'Durham';
+$states["East Sussex"] = 'East Sussex';
+$states["Essex"] = 'Essex';
+$states["Gloucestershire"] = 'Gloucestershire';
+$states["Hampshire"] = 'Hampshire';
+$states["Herefordshire"] = 'Herefordshire';
+$states["Hertfordshire"] = 'Hertfordshire';
+$states["Isle of Wight"] = 'Isle of Wight';
+$states["Kent"] = 'Kent';
+$states["Lancashire"] = 'Lancashire';
+$states["Leicestershire"] = 'Leicestershire';
+$states["Lincolnshire"] = 'Lincolnshire';
+$states["London"] = 'London';
+$states["Merseyside"] = 'Merseyside';
+$states["Middlesex"] = 'Middlesex';
+$states["Norfolk"] = 'Norfolk';
+$states["Northamptonshire"] = 'Northamptonshire';
+$states["Northumberland"] = 'Northumberland';
+$states["North Humberside"] = 'North Humberside';
+$states["North Yorkshire"] = 'North Yorkshire';
+$states["Nottinghamshire"] = 'Nottinghamshire';
+$states["Oxfordshire"] = 'Oxfordshire';
+$states["Rutland"] = 'Rutland';
+$states["Shropshire"] = 'Shropshire';
+$states["Somerset"] = 'Somerset';
+$states["South Humberside"] = 'South Humberside';
+$states["South Yorkshire"] = 'South Yorkshire';
+$states["Staffordshire"] = 'Staffordshire';
+$states["Suffolk"] = 'Suffolk';
+$states["Surrey"] = 'Surrey';
+$states["Tyne and Wear"] = 'Tyne and Wear';
+$states["Warwickshire"] = 'Warwickshire';
+$states["West Midlands"] = 'West Midlands';
+$states["West Sussex"] = 'West Sussex';
+$states["West Yorkshire"] = 'West Yorkshire';
+$states["Wiltshire"] = 'Wiltshire';
+$states["Worcestershire"] = 'Worcestershire';
+
+
+// ----------------------------------
+//   Wales
+
+$states["xx91"]    = "";
+$states["GROUP5"] = "Wales";
+$states["Clwyd"] = 'Clwyd';
+$states["Dyfed"] = 'Dyfed';
+$states["Gwent"] = 'Gwent';
+$states["Gwynedd"] = 'Gwynedd';
+$states["Mid Glamorgan"] = 'Mid Glamorgan';
+$states["Powys"] = 'Powys';
+$states["South Glamorgan"] = 'South Glamorgan';
+$states["West Glamorgan"] = 'West Glamorgan';
+
+
+// ----------------------------------
+//   Scotland
+
+$states["xx6"]    = "";
+$states["GROUP6"] = "Scotland";
+$states["Aberdeenshire"] = 'Aberdeenshire';
+$states["Angus"] = 'Angus';
+$states["Argyll"] = 'Argyll';
+$states["Ayrshire"] = 'Ayrshire';
+$states["Banffshire"] = 'Banffshire';
+$states["Berwickshire"] = 'Berwickshire';
+$states["Bute"] = 'Bute';
+$states["Caithness"] = 'Caithness';
+$states["Clackmannanshire"] = 'Clackmannanshire';
+$states["Dumfriesshire"] = 'Dumfriesshire';
+$states["Dunbartonshire"] = 'Dunbartonshire';
+$states["East Lothian"] = 'East Lothian';
+$states["Fife"] = 'Fife';
+$states["Inverness-shire"] = 'Inverness-shire';
+$states["Kincardineshire"] = 'Kincardineshire';
+$states["Kinross-shire"] = 'Kinross-shire';
+$states["Kirkcudbrightshire"] = 'Kirkcudbrightshire';
+$states["Lanarkshire"] = 'Lanarkshire';
+$states["Midlothian"] = 'Midlothian';
+$states["Moray"] = 'Moray';
+$states["Nairnshire"] = 'Nairnshire';
+$states["Orkney"] = 'Orkney';
+$states["Peeblesshire"] = 'Peeblesshire';
+$states["Perthshire"] = 'Perthshire';
+$states["Renfrewshire"] = 'Renfrewshire';
+$states["Ross-shire"] = 'Ross-shire';
+$states["Roxburghshire"] = 'Roxburghshire';
+$states["Selkirkshire"] = 'Selkirkshire';
+$states["Shetland"] = 'Shetland';
+$states["Stirlingshire"] = 'Stirlingshire';
+$states["Sutherland"] = 'Sutherland';
+$states["West Lothian"] = 'West Lothian';
+$states["Wigtownshire"] = 'Wigtownshire';
+
+
+// ----------------------------------
+//   Northern Ireland
+
+$states["xx7"]    = "";
+$states["GROUP7"] = "Northern Ireland";
+$states["Antrim"] = 'Antrim';
+$states["Armagh"] = 'Armagh';
+$states["Down"] = 'Down';
+$states["Fermanagh"] = 'Fermanagh';
+$states["Londonderry"] = 'Londonderry';
+$states["Tyrone"] = 'Tyrone';
+$states["xx3"]    = "";
+$states["GROUP4"] = "International";
+$states["INT"]    = "International";
+
+
+// ==================================
+//   Stop editing here!
+
+
+return $states;
