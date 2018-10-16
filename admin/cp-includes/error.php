a9cdb20604f21aa16a3bac71b40602a80e3ede10
bjornthv <bjornthv@users.noreply.github.com>
2018-10-16 05:02:50 +0200

    Update error.php

--- admin/cp-includes/error.php
+++ admin/cp-includes/error.php
@@ -1,1 +1,11 @@
-<div id="topblue" class="fonts small">    <div class="holder">Page Not Found</div></div><div id="mainsection">    <div id="big_warning"></div>    <div id="big_warning_right">        <h1>Page Not Found</h1>        <p>Zenguin tried to find that page but failed. Are you sure it exists?</p>    </div></div>
+<div id="topblue" class="fonts small">
+    <div class="holder">Page Not Found</div>
+</div>
+
+<div id="mainsection">
+    <div id="big_warning"></div>
+    <div id="big_warning_right">
+        <h1>Page Not Found</h1>
+        <p>Zenguin tried to find that page but failed. Are you sure it exists?</p>
+    </div>
+</div>
